267c8c3cce46381727afc57dc2c26d2adc294ab7
Eric VanArtsdalen <eric.vanartsdalen@hotmail.com>
2025-09-12 18:34:57 +0100

    Minro updates

--- AspireApp.AppHost/AppHost.cs
+++ AspireApp.AppHost/AppHost.cs
@@ -5,32 +5,9 @@ using System.Diagnostics;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Check if Docker is running  (See if we can somehow move the function to Extensions later)
-// FRITZ: Removed as it is now part of .NET Aspire
-
 // FRITZ: Config with .NET Aspire
-var aiModel = builder.AddParameterFromConfiguration("AI_Model", "AI_Model");
-var aiEndpoint = builder.AddParameterFromConfiguration("AI_Endpoint", "AI_Endpoint");
-
-
-
-// Read Configs - setup details to pass through.
-// (Not sure of the best way to accomplish this)
-// CONFIGURATIONS  
-// var environmentName = builder.Environment.EnvironmentName;
-// var baseProjectDirectory = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..");
-// Console.WriteLine($"Base project directory: {baseProjectDirectory}");
-// var blazorProjectPath = Path.Combine(baseProjectDirectory, "AspireApp.Web");
-// var baseSettingsPath = Path.Combine(blazorProjectPath, "appsettings.json");
-// var envSettingsPath = Path.Combine(blazorProjectPath, $"appsettings.{environmentName}.json");
-// var configuration = new ConfigurationBuilder()
-// 	.SetBasePath(Directory.GetCurrentDirectory())
-// 	.AddJsonFile(baseSettingsPath, optional: false)
-// 	.AddJsonFile(envSettingsPath, optional: true)
-// 	.Build();
-// // Fetch and set values for ollama endpoint & model
-// var aiModel = configuration.GetValue<string>("AI_Model") ?? "phi3:latest";
-// var aiEndpoint = configuration.GetValue<string>("AI_Endpoint") ?? "http://localhost:11434";
+var aiModel = builder.AddParameterFromConfiguration("AI-Model", "AI-Model");
+var aiEndpoint = builder.AddParameterFromConfiguration("AI-Endpoint", "AI-Endpoint");
 
 // API Service
 var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
@@ -39,7 +16,7 @@ var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
 // TODO add AI Ollama & AI Model from configs
 // SETUP OLLAMA & MODEL CONTAINERS
 var ollama = builder.AddOllama("ollama")
-	.WithAnnotation(new ContainerImageAnnotation { Image = "ollama/ollama", Tag = "0.9.5" })
+	.WithAnnotation(new ContainerImageAnnotation { Image = "ollama/ollama", Tag = "0.11.4" })
 	.WithDataVolume()
 	.WithContainerRuntimeArgs("--gpus", "all");
 var appmodel = ollama.AddModel("chat", aiModel.Resource.Value);
